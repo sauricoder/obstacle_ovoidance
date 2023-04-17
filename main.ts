4008fda10a4ea81ebfea7f54e306222da5b67f45
sauricoder <122543575+sauricoder@users.noreply.github.com>
2023-04-17 09:38:47 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+let sonar = 0
+basic.showIcon(IconNames.Happy)
+wuKong.setAllMotor(100, 100)
+basic.forever(function () {
+    sonar = sonarbit.sonarbit_distance(Distance_Unit.Distance_Unit_cm, DigitalPin.P1)
+    if (sonar < 20 && sonar > 1) {
+        wuKong.setAllMotor(50, -100)
+        basic.pause(randint(0, 10))
+    } else {
+        wuKong.setAllMotor(100, 100)
+    }
+})
